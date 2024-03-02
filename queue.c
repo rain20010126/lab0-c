e562198181610e1fe64c5a9c9d246f89335e3f6f
ShengYu Chang <rain010126@gmail.com>
2024-03-02 14:21:19 +0000

    Add q_remove_head

--- queue.c
+++ queue.c
@@ -87,8 +87,18 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    element_t *node = list_first_entry(head, element_t, list);
+
+    if (sp) {
+        memcpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(head->next);
+    return node;
 }
+
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
