e1d429116ac2cf5ad3bfd08e22d88a0c538a1b27
ShengYu Chang <rain010126@gmail.com>
2024-03-03 15:50:44 +0000

    Add q_reverse

--- queue.c
+++ queue.c
@@ -195,7 +195,15 @@ void q_swap(struct list_head *head)
 
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    struct list_head *first = head->next;
+    struct list_head *tail = head->prev;
+    while (tail->prev != head) {
+        list_move(first, tail);
+        first = head->next;
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
